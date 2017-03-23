366dddc371417dc79d4ac697b0b1cff799d90d22
WallaceDunn <18011755411@qq.com>
2017-03-23 09:24:52 +0800

    MyShop_Demo

---
+++ app/src/main/java/com/gaojia/myshop_demo/base/BaseFragment.java
@@ -0,0 +1,8 @@
+package com.gaojia.myshop_demo.base;
+
+/**
+ * Created by Administrator on 2017/3/22 0022.
+ */
+
+public class BaseFragment {
+}
---
+++ app/src/main/java/com/gaojia/myshop_demo/mine/MineActivity.java
@@ -0,0 +1,11 @@
+package com.gaojia.myshop_demo.mine;
+
+import com.gaojia.myshop_demo.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/3/22 0022.
+ */
+
+public class MineActivity extends BaseActivity{
+
+}
